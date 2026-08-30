6b0c2c465b82e6f94e54c8e336fbbcde7c4493ef
v31l0x1 <pmani9505@gmail.com>
2026-08-30 21:42:09 +0530

    added proc terminate

---
+++ ProcTerminate_1/ProcTermClient/main.c
@@ -0,0 +1,124 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include "defines.h"
+
+#define IOCTL_TERMINATE_PROC CTL_CODE( 0x8001, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS )
+
+typedef struct {
+	DWORD Pid;
+} ProcTerm;;
+
+DWORD findProc(LPCWSTR processName) {
+
+	DWORD pid = 0;
+
+	HMODULE hNtdll = GetModuleHandle(L"ntdll.dll");
+	if (!hNtdll) {
+		wprintf(L"[-] Failed to get handle to ntdll.dll\n");
+		return 0;
+	}
+	
+	fnNtQuerySystemInformation NtQuerySystemInformation = (fnNtQuerySystemInformation)GetProcAddress(hNtdll, "NtQuerySystemInformation");
+	if (!NtQuerySystemInformation) {
+		wprintf(L"[-] Failed to get address of NtQuerySystemInformation\n");
+		return 0;
+	}
+
+	NTSTATUS status;
+	ULONG returnLength = 0;
+	status = NtQuerySystemInformation(SystemProcessInformation, NULL, 0, &returnLength);
+
+	if (status != STATUS_INFO_LENGTH_MISMATCH) {
+		wprintf(L"[-] NtQuerySystemInformation failed to get required buffer size\n");
+		return 0;
+	}
+
+	PVOID buffer = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, returnLength);
+	if (!buffer) {
+		wprintf(L"[-] Failed to allocate memory for process information\n");
+		return 0;
+	}
+
+	status = NtQuerySystemInformation(SystemProcessInformation, buffer, returnLength, &returnLength);
+	
+	if (!NT_SUCCESS(status)) {
+		wprintf(L"[-] NtQuerySystemInformation failed\n");
+		HeapFree(GetProcessHeap(), 0, buffer);
+		return 0;
+	}
+
+	PSYSTEM_PROCESS_INFORMATION pInfo = (PSYSTEM_PROCESS_INFORMATION)buffer;
+	while (TRUE) {
+		if (pInfo->ImageName.Buffer && _wcsicmp(pInfo->ImageName.Buffer, processName) == 0) {
+			pid = (DWORD)(ULONG_PTR)pInfo->UniqueProcessId;
+			break;
+		}
+
+		if (pInfo->NextEntryOffset == 0) {
+			break;
+		}
+
+		pInfo = (PSYSTEM_PROCESS_INFORMATION)((PBYTE)pInfo + pInfo->NextEntryOffset);
+
+	}
+
+	HeapFree(GetProcessHeap(), 0, buffer);
+	return pid;
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+	PCWSTR processName = L"notepad.exe";
+
+	DWORD pid = findProc(processName);
+
+	if (pid == 0) {
+		wprintf(L"[-] Process %s not found\n", processName);
+		return 1;
+	}
+
+	printf("[+] Found process %s with PID: %lu\n", processName, pid);
+
+	HANDLE hDevice = CreateFile(
+		L"\\\\.\\ProcTerminate",
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		0,
+		NULL
+	);
+
+	if (hDevice == INVALID_HANDLE_VALUE) {
+		wprintf(L"[-] Failed to open device\n");
+		return 1;
+	}
+
+	ProcTerm procTerm;
+	procTerm.Pid = pid;
+
+	DWORD bytesReturned;
+	BOOL success = DeviceIoControl(
+		hDevice,
+		IOCTL_TERMINATE_PROC,
+		&procTerm,
+		sizeof(procTerm),
+		NULL,
+		0,
+		&bytesReturned,
+		NULL
+	);
+
+	if (!success) {
+		wprintf(L"[-] DeviceIoControl failed\n");
+		CloseHandle(hDevice);
+		return 1;
+	}
+
+	wprintf(L"[+] Killed %s with PID: %lu\n", processName, pid);
+
+	return 0;
+}
---
+++ ProcTerminate_1/ProcTerminate_1/Driver.cpp
@@ -0,0 +1,122 @@
+#include <ntddk.h>
+#include <ntstatus.h>
+
+#define IOCTL_TERMINATE_PROC CTL_CODE( 0x8001, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS )
+#define PROCESS_TERMINATE 0x0001
+
+typedef unsigned long DWORD;
+
+typedef struct {
+	DWORD Pid;
+} ProcTerm;
+
+NTSTATUS DeviceCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS DeviceIoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+VOID UnloadDriver(PDRIVER_OBJECT DriverObject);
+
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING)
+{
+	UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\Device\\ProcTerminate");
+	UNICODE_STRING SymbolicLinkName = RTL_CONSTANT_STRING(L"\\??\\ProcTerminate");
+	NTSTATUS ntStatus;
+	PDEVICE_OBJECT DeviceObject = NULL;
+
+	ntStatus = IoCreateDevice(
+		DriverObject,
+		0,
+		&DeviceName,
+		FILE_DEVICE_UNKNOWN,
+		FILE_DEVICE_SECURE_OPEN,
+		FALSE,
+		&DeviceObject
+	);
+
+	if (!NT_SUCCESS(ntStatus)) {
+		DbgPrint("[-] Failed to create device");
+		return ntStatus;
+	}
+
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = DeviceCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = DeviceCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DeviceIoControl;
+	DriverObject->DriverUnload = UnloadDriver;
+
+	ntStatus = IoCreateSymbolicLink(&SymbolicLinkName, &DeviceName);
+
+	if (!NT_SUCCESS(ntStatus)) {
+		DbgPrint("[-] Failed to create symbolic link");
+		IoDeleteDevice(DeviceObject);
+		return ntStatus;
+	}
+
+	return STATUS_SUCCESS;
+}
+
+
+NTSTATUS DeviceCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+	UNREFERENCED_PARAMETER(DeviceObject);
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+VOID UnloadDriver(PDRIVER_OBJECT DriverObject)
+{
+	PDEVICE_OBJECT DeviceObject = DriverObject->DeviceObject;
+	UNICODE_STRING SymbolicLinkName = RTL_CONSTANT_STRING(L"\\??\\ProcTerminate");
+
+	IoDeleteSymbolicLink(&SymbolicLinkName);
+	if (DeviceObject != NULL) {
+		IoDeleteDevice(DeviceObject);
+	}
+
+	DbgPrint("[+] Driver unloaded successfully");
+}
+
+
+NTSTATUS DeviceIoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	PIO_STACK_LOCATION irpSp = IoGetCurrentIrpStackLocation(Irp);
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+	UNREFERENCED_PARAMETER(DeviceObject);
+
+	if (irpSp->Parameters.DeviceIoControl.IoControlCode == IOCTL_TERMINATE_PROC) {
+
+		ProcTerm* procTerm = (ProcTerm*)Irp->AssociatedIrp.SystemBuffer;
+
+		if (procTerm != NULL) {
+			HANDLE procHandle;
+			OBJECT_ATTRIBUTES objAttr;
+			CLIENT_ID clientId;
+			clientId.UniqueProcess = UlongToHandle(procTerm->Pid);
+			clientId.UniqueThread = NULL;
+			InitializeObjectAttributes(&objAttr, NULL, 0, NULL, NULL);
+
+			ntStatus = ZwOpenProcess(&procHandle, PROCESS_TERMINATE, &objAttr, &clientId);
+
+			if (NT_SUCCESS(ntStatus)) {
+				ntStatus = ZwTerminateProcess(procHandle, 0);
+				ZwClose(procHandle);
+			}
+			else {
+				DbgPrint("[-] Failed to open process with PID: %lu", procTerm->Pid);
+			}
+			ntStatus = STATUS_SUCCESS;
+		}
+		else {
+			DbgPrint("[-] Invalid input buffer");
+			ntStatus = STATUS_INVALID_PARAMETER;
+		}
+	}
+	else {
+		DbgPrint("[-] Invalid IOCTL code");
+		ntStatus = STATUS_INVALID_DEVICE_REQUEST;
+	}
+
+	Irp->IoStatus.Status = ntStatus;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return ntStatus;
+}
