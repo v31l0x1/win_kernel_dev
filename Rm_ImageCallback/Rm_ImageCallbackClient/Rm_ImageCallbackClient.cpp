05397493819cdda77b4daa880f2e3a5dce80e951
v31l0x1 <pmani9505@gmail.com>
2026-09-06 15:18:54 +0530

    updated client code

--- Rm_ImageCallback/Rm_ImageCallbackClient/Rm_ImageCallbackClient.cpp
+++ Rm_ImageCallback/Rm_ImageCallbackClient/Rm_ImageCallbackClient.cpp
@@ -1,20 +1,138 @@
-// Rm_ImageCallbackClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <Windows.h>
+#include <stdio.h>
+#include <winioctl.h>
 
-#include <iostream>
+#define IOCTL_READ_IMAGE_CALLBACK CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_RM_IMAGE_CALLBACK CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#define ENTRY_SIZE 56
+#define MAX_ENTRIES 64
+
+typedef struct _IMAGE_CALLBACK_DATA {
+	int Index;
+} IMAGE_CALLBACK_DATA, * PIMAGE_CALLBACK_DATA;
+
+VOID EnumCallbacks(BYTE* buffer, DWORD size) {
+	DWORD count = size;
+	ULONG activeCount = 0;
+	ULONG removedCount = 0;
+
+	printf("\n[+] Image Load Callbacks:\n");
+
+	while (count >= ENTRY_SIZE)
+	{
+		ULONG64 index = *(ULONG64*)buffer;
+		buffer += 8;
+		ULONG64 addr = *(ULONG64*)buffer;
+		buffer += 8;
+		PCHAR ModuleName = (PCHAR)buffer;
+		buffer += 32;
+		ULONG64 ModuleBase = *(ULONG64*)buffer;
+		buffer += 8;
+
+		count -= ENTRY_SIZE;
+
+		if (addr != 0) {
+			printf("    [%02llu] 0x%016llx  (%s + 0x%llx)\n", index, addr, ModuleName, ModuleBase);
+			activeCount++;
+		}
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+HANDLE GetDriverHandle(VOID)
+{
+	HANDLE hDevice = CreateFileW(
+		L"\\\\.\\Rm_ImageCallback",
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+	if (hDevice == INVALID_HANDLE_VALUE) {
+		printf("Failed to open device: %d\n", GetLastError());
+		return NULL;
+	}
+	return hDevice;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+BOOL RemoveCallbacks(HANDLE hDevice, ULONG index)
+{
+	IMAGE_CALLBACK_DATA pImageCallbackData = { 0 };
+	pImageCallbackData.Index = index;
+
+	DWORD bytesReturned = 0;
+	printf("[+] Removing image callback at index %lu...\n", index);
+
+	if (!DeviceIoControl(
+		hDevice,
+		IOCTL_RM_IMAGE_CALLBACK,
+		&pImageCallbackData, sizeof(pImageCallbackData),
+		NULL, 0,
+		&bytesReturned,
+		NULL
+	)) {
+		printf("Failed to remove callback: %d\n", GetLastError());
+		return FALSE;
+	}
+
+	printf("[+] Removed callback at index %lu.\n", index);
+
+	return TRUE;
+}
+
+
+BOOL ListCallbacks(HANDLE hDevice)
+{
+	BYTE Buffer[4096] = { 0 };
+	DWORD bytesReturned = 0;
+
+	if (!DeviceIoControl(
+		hDevice,
+		IOCTL_READ_IMAGE_CALLBACK,
+		NULL, 0,
+		Buffer, sizeof(Buffer),
+		&bytesReturned,
+		NULL
+	)) {
+		printf("Failed to read from device: %d\n", GetLastError());
+		return FALSE;
+	}
+
+	if (bytesReturned == 0) {
+		printf("No thread callbacks found.\n");
+		return FALSE;
+	}
+
+	EnumCallbacks(Buffer, bytesReturned);
+	return TRUE;
+}
+
+int main(int argc, char* argv[])
+{
+	HANDLE hDevice = GetDriverHandle();
+	if (hDevice == INVALID_HANDLE_VALUE) {
+		return 1;
+	}
+
+	if (argc >= 2) {
+		ULONG index = strtoul(argv[1], NULL, 10);
+
+		if (index >= MAX_ENTRIES) {
+			printf("Index must be between 0 and %d\n", MAX_ENTRIES - 1);
+			return 1;
+		}
+
+		if (!RemoveCallbacks(hDevice, index)) {
+			return 1;
+		}
+	}
+
+	if (!ListCallbacks(hDevice)) {
+		return 1;
+	}
+
+	CloseHandle(hDevice);
+	return 0;
+}
